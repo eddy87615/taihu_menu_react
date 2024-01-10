8176b4c66d02b981274ddcd22195f98b792540d0
EddyChen <130333523+eddy87615@users.noreply.github.com>
2024-01-10 17:12:40 +0900

    index.js を更新

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ const beer = [
       '麦芽由来の天然発酵による9.99%の高アルコール系。カクテルの味わいをビールで表現しました。オンザロックスタイルで飲んで頂きます。',
     INprice: '¥1,200	( 500ml_Can)',
     OUTprice: '¥1,020	( 500ml_Can)',
-    soldout: false,
+    soldout: true,
     series: '9.99',
   },
   {
